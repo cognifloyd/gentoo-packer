034a4b6c7076a888f90e04874510ebf1df83f75f
Jacob Floyd <cognifloyd@gmail.com>
2014-06-04 22:27:58 -0400

    Use New York time

--- scripts/timezone.sh
+++ scripts/timezone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 chroot /mnt/gentoo /bin/bash <<'EOF'
-ln -snf /usr/share/zoneinfo/UTC /etc/localtime
-echo UTC > /etc/timezone
+ln -snf /usr/share/zoneinfo/America/New_York /etc/localtime
+echo America/New_York > /etc/timezone
 EOF
